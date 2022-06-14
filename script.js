e17794fe5d1d6e4c2abcd4a5ee0175b3a8a66f5b
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-06-14 23:01:39 +0200

    init project testing with jest

---
+++ script.js
@@ -0,0 +1,15 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavouritecats.com'
+];
+
+const googleSearch = (searchinput) => {
+    const matches = googleDatabase.filter(website => website.includes(searchinput));
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+
+console.log(googleSearch('cat'));
