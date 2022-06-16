f7e5bbf4f5d3aa4455bff4a738016c102d518751
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-06-16 11:39:11 +0200

    writing first tests

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const googleDatabase = [
     'myfavouritecats.com'
 ];
 
-const googleSearch = (searchinput) => {
-    const matches = googleDatabase.filter(website => website.includes(searchinput));
+const googleSearch = (searchinput, db) => {
+    const matches = db.filter(website => website.includes(searchinput));
     return matches.length > 3 ? matches.slice(0, 3) : matches;
 }
 
-console.log(googleSearch('cat'));
+// console.log(googleSearch('cat', googleDatabase));
+
+module.exports = googleSearch;
